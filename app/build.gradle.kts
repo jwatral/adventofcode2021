6f5661f677144b049bf3a8fbccdae2ebfaf47583
Jarosław Watral <jwatral@users.noreply.github.com>
2021-12-03 23:44:56 +0100

    added simple caching for input files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,9 +9,6 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.5.31"
-
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
 }
 
 repositories {
@@ -26,17 +23,9 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:30.1.1-jre")
-
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-}
-
-application {
-    // Define the main class for the application.
-    mainClass.set("adventofcode2021.AppKt")
 }
--- app/src/test/kotlin/adventofcode2021/common/utils.kt
+++ app/src/test/kotlin/adventofcode2021/common/utils.kt
@@ -4,9 +4,13 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.file.Path
 import java.util.*
 import kotlin.io.path.Path
+import kotlin.io.path.exists
 import kotlin.io.path.readBytes
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
 
 
 val RESOURCES_DIR = Path("src", "test", "resources")
@@ -17,7 +21,17 @@ val SESSION_TOKEN = Properties().apply {
 
 val httpClient: HttpClient = HttpClient.newBuilder().build()
 
-fun getInputForDay(dayNum: Int): String {
+fun getInputForDay(dayNum: Int): String = pathForInputFile(dayNum).let { inputPath ->
+    if(inputPath.exists()) inputPath.readText()
+    else downloadInputForDay(dayNum).also { saveInputFile(inputPath, it) }
+}
+
+private fun saveInputFile(path: Path, input: String): String = input.also { path.writeText(it) }
+
+private fun pathForInputFile(dayNum: Int): Path =
+    RESOURCES_DIR.resolve("task-inputs").resolve("$dayNum.txt")
+
+private fun downloadInputForDay(dayNum: Int): String {
     val request = HttpRequest.newBuilder()
         .GET()
         .uri(URI.create("https://adventofcode.com/2021/day/$dayNum/input"))
