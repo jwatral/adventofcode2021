43e74fff87616c05b56228da22efeb288e29945f
Jarosław Watral <jwatral@users.noreply.github.com>
2021-12-07 23:16:56 +0100

    day07 part 1 solved

---
+++ app/src/main/kotlin/adventofcode2021/SolutionDay07.kt
@@ -0,0 +1,44 @@
+package adventofcode2021
+
+import adventofcode2021.common.CommonTask
+import kotlin.math.abs
+
+class Day07Part1 : CommonTask<List<Int>, Int>(
+    dayNum = 7,
+    example = example,
+    inputConverter = inputConverter,
+    exampleResult = 37,
+    taskResult = 351901,
+) {
+
+    override fun calculateResult(initialPositions: List<Int>): Int {
+        val minValue = initialPositions.minOrNull() ?: 0
+        val maxValue = initialPositions.maxOrNull() ?: 0
+        return (minValue..maxValue).minOfOrNull { checkedPosition ->
+            initialPositions.sumOf { abs(it - checkedPosition) }
+        } ?: 0
+    }
+}
+
+class Day07Part2 : CommonTask<List<Int>, Int>(
+    dayNum = 7,
+    example = example,
+    inputConverter = inputConverter,
+    exampleResult = 168,
+    taskResult = 351901,
+) {
+
+    override fun calculateResult(initialPositions: List<Int>): Int {
+        val minValue = initialPositions.minOrNull() ?: 0
+        val maxValue = initialPositions.maxOrNull() ?: 0
+        return (minValue..maxValue).minOfOrNull { checkedPosition ->
+            initialPositions.sumOf { abs(it - checkedPosition) }
+        } ?: 0
+    }
+}
+
+private val inputConverter: (String) -> List<Int> = { s ->
+    s.trim().split(",").map { it.toInt() }
+}
+
+private val example = "16,1,2,0,4,2,7,1,2,14"
