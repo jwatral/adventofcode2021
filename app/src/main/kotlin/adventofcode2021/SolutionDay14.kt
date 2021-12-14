71fa237e7bad86c0efbc46266a667252a9fcd5a2
Jarosław Watral <jwatral@users.noreply.github.com>
2021-12-14 23:37:49 +0100

    day 14 part 2 solved

--- app/src/main/kotlin/adventofcode2021/SolutionDay14.kt
+++ app/src/main/kotlin/adventofcode2021/SolutionDay14.kt
@@ -7,42 +7,57 @@ private object Day14 : Day<Day14.Input> {
     override val dayNum = 14
     override fun inputConverter(input: String): Input =
         input.trim().lines().partition { it.contains(" -> ") }
-        .let { (insertions, template) ->
-            Input(template[0], insertions.map { it.split(" -> ") }.map{ Insertion(it[0][0] to it[0][1], it[1][0]) })
-        }
+            .let { (insertions, template) ->
+                Input(
+                    template[0],
+                    insertions.map { it.split(" -> ") }.map { Insertion(it[0][0] to it[0][1], it[1][0]) })
+            }
 
     class Day14Part1 : CommonPartTest<Input>(
         day = this,
         exampleResult = "1588",
         taskResult = "3259",
     ) {
-        override fun calculateResult(input: Input): String =
-            (1..10).fold(input.polymerTemplate.toCharArray().toList()) { acc, _ -> step(acc, input.insertions) }
-                .groupBy { it }
-                .let {
-                    val sorted = it.values.map { it.size }.sorted()
-                    sorted.last() - sorted.first()
-                }.toString()
-
-        private fun step(input: List<Char>, insertions: List<Insertion>): List<Char> =
-            input.zipWithNext().mapIndexed { index, pair -> insertions.find { it.pair == pair }?.afterInsertion(index) ?: pair.toList() }.flatten()
+        override fun calculateResult(input: Input): String = Day14.calculateResult(input, 10)
     }
 
     class Day14Part2 : CommonPartTest<Input>(
         day = this,
-        exampleResult = "42",
-        taskResult = "42",
+        exampleResult = "2188189693529",
+        taskResult = "3459174981021",
     ) {
-        override fun calculateResult(input: Input): String = TODO()
+        override fun calculateResult(input: Input): String = Day14.calculateResult(input, 40)
     }
 
-    data class Input(val polymerTemplate: String, val insertions: List<Insertion>)
+    fun calculateResult(input: Input, steps: Int): String {
+        var freqMap =
+            input.polymerTemplate.zipWithNext()
+                .groupingBy { it }
+                .eachCount()
+                .mapValues { it.value.toLong() }
+                .toMutableMap()
+
+        repeat(steps) {
+            val newFreqMap = mutableMapOf<Pair<Char, Char>, Long>()
+            for (pair in freqMap.keys) {
+                val (first, second) = input.insertions.find { it.pair == pair }?.afterInsertion()!!
+                newFreqMap[first] = newFreqMap.getOrDefault(first, 0) + freqMap[pair]!!
+                newFreqMap[second] = newFreqMap.getOrDefault(second, 0) + freqMap[pair]!!
+            }
+            freqMap = newFreqMap
+        }
+
+        return freqMap
+            .map { it.key.first to it.value }
+            .groupBy({ it.first }, { it.second })
+            .mapValues { it.value.sum() + if (it.key == input.lastChar) 1 else 0 }
+            .values.sorted().let { it.last() - it.first() }.toString()
+    }
+
+    data class Input(val polymerTemplate: String, val insertions: List<Insertion>, val lastChar: Char = polymerTemplate.last())
 
     data class Insertion(val pair: Pair<Char, Char>, val toInsert: Char) {
-        fun afterInsertion(index: Int) = when(index) {
-            0 -> listOf(pair.first, toInsert, pair.second)
-            else -> listOf(toInsert, pair.second)
-        }
+        fun afterInsertion() = listOf(pair.first to toInsert, toInsert to pair.second)
     }
 
     override val example = """
